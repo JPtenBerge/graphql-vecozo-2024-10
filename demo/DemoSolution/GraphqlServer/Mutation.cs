d0c1b1d8af90377dcb8cc15dcfa8d8077fd1f38e
JP <jp@jp.nl>
2024-10-22 10:24:31 +0200

    Blazor WebAssembly-project gegenereerd

--- demo/DemoSolution/GraphqlServer/Mutation.cs
+++ demo/DemoSolution/GraphqlServer/Mutation.cs
@@ -6,7 +6,7 @@ namespace GraphqlServer;
 
 public class Mutation
 {
-    public Movie AddMovie(AddMovieInput input, MovieRepository movieRepository)
+    public Movie AddMovie(AddMovieInput input, IMovieRepository movieRepository)
     {
         var movie = movieRepository.Add(input.Title, input.ReleaseYear, input.DirectorId);
         return movie;
